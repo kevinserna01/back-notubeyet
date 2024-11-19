2c981c29a30de39de0d00a2da69768390f8a69cb
kevinserna01 <kevinfernandoserna11@gmail.com>
2024-11-19 14:07:33 -0500

    funcionaaa

--- aws/upload.js
+++ aws/upload.js
@@ -1,9 +1,9 @@
 const multer = require('multer');
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client } = require('@aws-sdk/client-s3');
 const multerS3 = require('multer-s3');
 require('dotenv').config();
 
-// Configurar AWS S3 (v3)
+// Configuración de AWS S3 (v3)
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -12,19 +12,23 @@ const s3Client = new S3Client({
   },
 });
 
+// Configuración de multer para cargar archivos a S3
 const upload = multer({
   storage: multerS3({
     s3: s3Client,
-    bucket: process.env.AWS_BUCKET_NAME,
+    bucket: process.env.AWS_BUCKET_NAME, // Asegúrate de que esta variable de entorno esté correctamente definida
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
     key: (req, file, cb) => {
       const uniqueName = `${Date.now().toString()}-${file.originalname}`;
-      cb(null, uniqueName);
+      cb(null, uniqueName); // Genera un nombre único para el archivo
     },
-    contentType: multerS3.AUTO_CONTENT_TYPE,
+    contentType: multerS3.AUTO_CONTENT_TYPE, // S3 detectará el tipo de contenido automáticamente
   }),
+  limits: {
+    fileSize: 50 * 1024 * 1024, // Límite de tamaño de archivo: 50MB
+  },
 });
 
 module.exports = upload;
