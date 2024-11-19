71a27a233e74af91825654902bf83c115ab44b59
kevinserna01 <kevinfernandoserna11@gmail.com>
2024-11-19 12:43:01 -0500

    mas espacio

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const { urlencoded, json } = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const router = require('./routes/tubeyet.routes.js'); // Cambia esta ruta según tus necesidades
+const router = require('./routes/tubeyet.routes.js'); // Asegúrate de que la ruta sea correcta
 
 const app = express();
 
-// Middleware para analizar datos codificados y JSON
-app.use(urlencoded({ extended: true }));
-app.use(json());
+// Middleware para analizar datos codificados y JSON con un límite ampliado
+app.use(json({ limit: '50mb' })); // Aumenta el límite para JSON
+app.use(urlencoded({ limit: '50mb', extended: true })); // Aumenta el límite para datos codificados
 
 // Configuración de CORS
-const allowedOrigins = ['https://front-notubeyet.vercel.app',];
+const allowedOrigins = ['https://front-notubeyet.vercel.app'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
     res.send('Bienvenido al backend de NotubeYet!');
 });
 
-// Utiliza las rutas de usuario (login, registro, etc.)
+// Utiliza las rutas del sistema
 app.use('/v1/tubeyet', router);
 
 // Iniciar el servidor
