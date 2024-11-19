ac35481e8b611106a9367b1042bb6d96a458c520
kevinserna01 <kevinfernandoserna11@gmail.com>
2024-11-18 21:16:40 -0500

    yaa

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { urlencoded, json } = require('express');
 const cors = require('cors');
+const path = require('path');
+const serveFavicon = require('serve-favicon'); // Importa serve-favicon
 require('dotenv').config();
 const router = require('./routes/tubeyet.routes.js'); // Cambia esta ruta según tus necesidades
 
 const app = express();
+
+// Middleware para servir el favicon
+app.use(serveFavicon(path.join(__dirname, 'public', 'favicon.ico'))); // Ajusta la ruta de tu favicon
 
 // Middleware para analizar datos codificados y JSON
 app.use(urlencoded({ extended: true }));
